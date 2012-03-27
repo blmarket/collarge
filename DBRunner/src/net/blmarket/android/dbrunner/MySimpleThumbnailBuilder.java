0500977df33a439945a9ffa2ac17b4bde4e0afcd
1000743 <1000743@SKT1000743PN001.SKT.AD>
2012-03-27 15:54:36 +0900

    MySimpleThumbnailBuilder를 만들었다.
    캐시 기능 없이 단순히 그림을 원하는 사이즈로 줄여서 썸네일을 만들어주는
    
    녀석이니 존나 느리겠지?

---
+++ DBRunner/src/net/blmarket/android/dbrunner/MySimpleThumbnailBuilder.java
@@ -0,0 +1,38 @@
+package net.blmarket.android.dbrunner;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+public class MySimpleThumbnailBuilder implements IThumbnailBuilder {
+
+	private static final int THUMBNAIL_HEIGHT = 150;
+
+	ContentResolver contentResolver;
+
+	public MySimpleThumbnailBuilder(ContentResolver contentResolver) {
+		this.contentResolver = contentResolver;
+	}
+
+	@Override
+	public Bitmap build(Uri uri) {
+		try {
+			Bitmap ret = MediaStore.Images.Media
+					.getBitmap(contentResolver, uri);
+
+			Float width = new Float(ret.getWidth());
+			Float height = new Float(ret.getHeight());
+			Float ratio = width / height;
+			ret = Bitmap.createScaledBitmap(ret,
+					(int) (THUMBNAIL_HEIGHT * ratio), THUMBNAIL_HEIGHT, true);
+			return ret;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+}
