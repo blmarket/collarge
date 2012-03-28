9fdcc926768f5c6ce85b1ec8d36862683c45d419
thinker1986 <thinker1986@gmail.com>
2012-03-28 10:01:51 +0900

    4. 망했

--- MapTest/src/skp/collarge/maptest/MainActivity.java
+++ MapTest/src/skp/collarge/maptest/MainActivity.java
@@ -1,15 +1,21 @@
 package skp.collarge.maptest;
 
+import android.media.ExifInterface;
 import android.os.Bundle;
 
 import com.google.android.maps.MapActivity;
 
 public class MainActivity extends MapActivity {
 
+	private String imageString= "/sdcard/DCIM/Camera/20120327_172630.jpg";
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		//ExifInterface exif = new ExifInterface(filename);
+		
 	}
 
 	@Override
