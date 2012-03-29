dbd8dc49892d32b228178a1c201027f724d214b5
thinker1986 <thinker1986@gmail.com>
2012-03-29 14:23:02 +0900

    죽겄

---
+++ Collarge/src/skp/collage/mapview/MyItemizedOverlay.java
@@ -0,0 +1,38 @@
+package skp.collage.mapview;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
+
+public class MyItemizedOverlay extends BalloonItemizedOverlay<OverlayItem> {
+
+	private ArrayList<OverlayItem> m_overlays = new ArrayList<OverlayItem>();
+	private Context c;
+	
+	public MyItemizedOverlay(Drawable defaultMarker, MapView mapView) {
+		super(boundCenterBottom(defaultMarker), mapView);
+		c = mapView.getContext();
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+		m_overlays.add(overlay);
+		populate();
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		return m_overlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return m_overlays.size();
+	}
+
+	
+	
+}
