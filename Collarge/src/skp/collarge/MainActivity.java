b1f143e089e2056446a74c50ee849a166f0aa91e
1000743 <1000743@SKT1000743PN001.SKT.AD>
2012-04-02 20:25:25 +0900

    Media Scan을 좀 덜 멍청하게 하도록 바꿈.

--- Collarge/src/skp/collarge/MainActivity.java
+++ Collarge/src/skp/collarge/MainActivity.java
@@ -4,12 +4,17 @@ import skp.collarge.event.EventManager;
 import skp.collarge.main.CollargeMain;
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.media.MediaScannerConnection.MediaScannerConnectionClient;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements MediaScannerConnectionClient {
+	MediaScannerConnection mConnection;
+	Handler mHandler = new Handler();
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,24 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main_start);
         
         Handler mHandler = new Handler();
+        AllTheEvil.initialize(getApplicationContext());
+        EventManager.getInstance();
+        
+    	mConnection = new MediaScannerConnection(this, this);
+    	mConnection.connect();
+    }
+
+	@Override
+	public void onMediaScannerConnected() {
+		mConnection.scanFile("/mnt/sdcard/Collarge", "*/*");
+		
+	}
+
+	@Override
+	public void onScanCompleted(String arg0, Uri arg1) {
+		System.out.println(arg0 + " " + arg1);
+		mConnection.disconnect();
+		
     	mHandler.postDelayed(new Runnable() {
     		public void run() { 
     			// 1.5초 뒤에 Menu 액티비티 전환
@@ -26,11 +49,5 @@ public class MainActivity extends Activity {
     			finish();
     		}
     	}, 1500);
-    	
-		sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://"
-	            + Environment.getExternalStorageDirectory()))); 		
-		
-        AllTheEvil.initialize(getApplicationContext());
-        EventManager.getInstance();
-    }
+	}
 }
