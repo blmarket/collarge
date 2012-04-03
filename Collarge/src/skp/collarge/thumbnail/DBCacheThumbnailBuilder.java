03bc6466fb18f1b71c60aa480b8293d787bc69d7
1000743 <1000743@SKT1000743PN001.SKT.AD>
2012-04-03 10:56:31 +0900

    MultiImageView에서 원본 image를 사용해서 메모리를 엄청 먹는 문제를 해결

--- Collarge/src/skp/collarge/thumbnail/DBCacheThumbnailBuilder.java
+++ Collarge/src/skp/collarge/thumbnail/DBCacheThumbnailBuilder.java
@@ -4,33 +4,50 @@ import java.io.ByteArrayOutputStream;
 
 import skp.collarge.AllTheEvil;
 import skp.collarge.db.MyDB;
+import skp.collarge.db.MyDB.Thumb;
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore.Images;
 
 public class DBCacheThumbnailBuilder implements IThumbnailBuilder {
 
+	Context context;
 	IThumbnailBuilder localBuilder;
 
 	public DBCacheThumbnailBuilder(Context context, IThumbnailBuilder builder) {
+		this.context = context;
 		this.localBuilder = builder;
+	}
+	
+	public DBCacheThumbnailBuilder(Context context) {
+		this.context = context;
+		localBuilder = new MySimpleThumbnailBuilder(context.getContentResolver());
 	}
 
 	@Override
 	public Bitmap build(Uri uri) {
 		String key = uri.toString();
-		byte[] cache = AllTheEvil.getInstance().getDB().getThumb(key);
+		Thumb cache = AllTheEvil.getInstance().getDB().getThumb(key);
 		Bitmap bmp = null;
 
 		if (cache != null) {
-			System.out.println("Trying to decode cache : " + cache.length);
-			bmp = BitmapFactory.decodeByteArray(cache, 0, cache.length);
+			bmp = BitmapFactory.decodeByteArray(cache.data, 0, cache.data.length);
 		}
 
 		if (bmp == null) {
 			bmp = localBuilder.build(uri);
+			String path = "";
+			Cursor c = Images.Media.query(context.getContentResolver(), uri, null);
+			if(c.moveToNext())
+			{
+				path = c.getString(c.getColumnIndex(Images.Media.DATA));
+				System.out.println("Resolved path : " + path);
+			}
+			c.close();
 			if (bmp == null)
 				return null;
 			System.out.println("Bitmap Created : " + bmp.getWidth() + " "
@@ -39,7 +56,7 @@ public class DBCacheThumbnailBuilder implements IThumbnailBuilder {
 			ByteArrayOutputStream stream = new ByteArrayOutputStream();
 			bmp.compress(CompressFormat.JPEG, 50, stream);
 			System.out.println("Stream size : " + stream.size());
-			AllTheEvil.getInstance().getDB().putThumb(key, stream.toByteArray());
+			AllTheEvil.getInstance().getDB().putThumb(key, stream.toByteArray(), path);
 			return bmp;
 		}
 		System.out.println("Bitmap Loaded : " + bmp.getWidth() + " "
--- Collarge/src/skp/collarge/view/MultiImageView.java
+++ Collarge/src/skp/collarge/view/MultiImageView.java
@@ -3,6 +3,7 @@ package skp.collarge.view;
 import skp.collarge.R;
 import skp.collarge.event.EventManager;
 import skp.collarge.event.IEvent;
+import skp.collarge.thumbnail.DBCacheThumbnailBuilder;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
@@ -14,14 +15,14 @@ import android.widget.ViewSwitcher;
 import android.widget.ViewSwitcher.ViewFactory;
 
 public class MultiImageView extends ViewSwitcher implements ViewFactory {
-	
+
 	private IEvent event;
 	private int index = 0;
-	
+
 	public MultiImageView(Context context, IEvent event) {
 		super(context);
 		this.event = event;
-		this.addView(makeView());		
+		this.addView(makeView());
 		postDelayed(new Zwitter(this), 2000);
 	}
 
@@ -30,8 +31,9 @@ public class MultiImageView extends ViewSwitcher implements ViewFactory {
 
 		// FIXME: get candidate list from somewhere, and shuffle that list.
 		ImageView imv = new ImageView(getContext());
-		
-		imv.setImageURI(event.getEventPhotoList().get(index++));
+
+		imv.setImageBitmap(new DBCacheThumbnailBuilder(getContext())
+				.build(event.getEventPhotoList().get(index++)));
 		imv.setLayoutParams(new FrameLayout.LayoutParams(240, 200));
 		imv.setScaleType(ScaleType.CENTER_CROP);
 		if (index == event.getEventPhotoList().size())
@@ -50,13 +52,13 @@ public class MultiImageView extends ViewSwitcher implements ViewFactory {
 		@Override
 		public void run() {
 			int current = view.getDisplayedChild();
-			if(view.getChildCount() < 2)
+			if (view.getChildCount() < 2)
 				view.addView(view.makeView());
 			// TODO: do proper animations.
-			//view.setInAnimation(AnimationUtils.loadAnimation(view.getContext(),
-			//		R.anim.push_left_in));
-			view.setOutAnimation(AnimationUtils.loadAnimation(view.getContext(),
-					R.anim.push_up));
+			// view.setInAnimation(AnimationUtils.loadAnimation(view.getContext(),
+			// R.anim.push_left_in));
+			view.setOutAnimation(AnimationUtils.loadAnimation(
+					view.getContext(), R.anim.push_up));
 			view.showNext();
 			view.postDelayed(this, 2000);
 
